c021d8c7cefa44b5f60997162245b97b059839ef
om333 <om123@gmail.com>
2025-01-30 13:50:59 +0530

    om

--- terraform/locals/local.tf
+++ terraform/locals/local.tf
@@ -1,5 +1,5 @@
 locals {
   ami_id = data.aws_ami.omnamahashivaya.id
   instance_type = "t2.micro"
-  name = "${var.project}-${var.environment}-${var.component}"
+  name = "${var.project}-${var.environment}-${var.component}-change"
 }
